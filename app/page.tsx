2b040c9768da0f27eb2268a29d42e41d795bd71c
Rydhlnst <66582308+Rydhlnst@users.noreply.github.com>
2024-11-17 20:00:16 +0700

    Navbar Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 const Home = () => {
-  return (
-    <>
-      <h1 className="h1-bold font-spaceGrotesk">Welcome the Next JS</h1>
-      <h1 className="h1-bold font-inter">Gacorrrr</h1>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
